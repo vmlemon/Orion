a7002b3bdd75907c5c96cd07f44bad78b608395a
root <root@fedora28.novalocal>
2019-09-08 12:11:35 +0000

    Rebuild with new compiler

--- kernel/rpb8/include/version.h
+++ kernel/rpb8/include/version.h
@@ -1,1 +1,22 @@
+\
+//							
+\
+// Autmatically generated.  Do not edit.		
+\
+//							
+\
+						
+\
+#define KERNEL_VERSION         0			
+\
+#define KERNEL_SUBVERSION      4			
+\
+#define KERNEL_SUBSUBVERSION   0			
+
+\
+#define KERNELGENDATE          SHUFFLE3(day:(108-100),month:(109-100),year:(2019-2000))			
+\
+#define __USER__               "root@fedora28.novalocal"				
+\
+#define __KERNELBUILDNO__      1				
 
