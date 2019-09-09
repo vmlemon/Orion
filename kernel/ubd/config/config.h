5b202d22f171d5f803c01e18871fa4f059248510
Tyson Key <tyson.key@gmail.com>
2019-09-09 20:16:43 +0100

    xBuild

--- kernel/ubd/config/config.h
+++ kernel/ubd/config/config.h
@@ -1,6 +1,6 @@
 /* Automatically generated, don't edit */
 /* Generated on: tyson-Lenovo-ideapad-120S-14IAP */
-/* At: Mon, 09 Sep 2019 12:33:27 +0000 */
+/* At: Mon, 09 Sep 2019 19:16:32 +0000 */
 /* Linux version 5.0.0-27-generic (buildd@lgw01-amd64-016) (gcc version 8.3.0 (Ubuntu 8.3.0-6ubuntu1)) #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 */
 
 /* Pistachio Kernel Configuration System */
@@ -14,8 +14,8 @@
 
 
 /* X86 Processor Architecture */
-#undef  CONFIG_SUBARCH_X32
-#define CONFIG_SUBARCH_X64 1
+#define CONFIG_SUBARCH_X32 1
+#undef  CONFIG_SUBARCH_X64
 
 
 /* Processor Type */
@@ -87,13 +87,13 @@
 #define CONFIG_X86_SYSENTER 1
 #define CONFIG_X86_PGE 1
 #define CONFIG_X86_FXSR 1
-#undef  CONFIG_IS_32BIT
+#define CONFIG_IS_32BIT 1
 #define CONFIG_X86_HTT 1
 #define CONFIG_X86_PAT 1
 #undef  CONFIG_PPC_BOOKE
-#define CONFIG_IS_64BIT 1
+#undef  CONFIG_IS_64BIT
 #undef  CONFIG_MULTI_ARCHITECTURE
-#define CONFIG_X86_EM64T 1
+#undef  CONFIG_X86_EM64T
 #undef  CONFIG_PPC_CACHE_L1_WRITETHROUGH
 #undef  CONFIG_PPC_TLB_INV_LOCAL
 #undef  CONFIG_PPC_CACHE_ICBI_LOCAL
