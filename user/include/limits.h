83392cb1464137df70acaff7ebf530cf5002c222
Tyson Key <tyson.key@gmail.com>
2019-09-13 05:55:02 +0100

    __SHRT_MIN/__SHRT_MIN

---
+++ user/include/limits.h
@@ -0,0 +1,12 @@
+#ifndef __SHRT_MAX
+#define	__SHRT_MAX	0x7fff		/* max value for a short */
+#endif
+
+#ifndef __SHRT_MIN
+#define	__SHRT_MIN	(-0x7fff - 1)	/* min value for a short */
+#endif
+ 
+
+#ifndef SHRT_MAX
+#define SHRT_MAX __SHRT_MIN
+#endif SHRT_MAX
---
+++ user/include/machine/_limits.h
@@ -0,0 +1,2 @@
+//#define	__SHRT_MAX	0x7fff		/* max value for a short */
+//#define	__SHRT_MIN	(-0x7fff - 1)	/* min value for a short */
