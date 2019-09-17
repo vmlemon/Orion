089eed84a17114037cf51c76d0f847f5921bcba1
root <root@fedora28.novalocal>
2019-09-17 01:56:41 +0000

    Disable asserts

--- kernel/p3/config/config.h
+++ kernel/p3/config/config.h
@@ -1,6 +1,6 @@
 /* Automatically generated, don't edit */
 /* Generated on: fedora28.novalocal */
-/* At: Tue, 17 Sep 2019 01:36:44 +0000 */
+/* At: Tue, 17 Sep 2019 01:56:23 +0000 */
 /* Linux version 4.16.3-301.fc28.ppc64 (mockbuild@buildvm-ppc64-06.ppc.fedoraproject.org) (gcc version 8.0.1 20180324 (Red Hat 8.0.1-0.20) (GCC)) #1 SMP Mon Apr 23 21:44:46 UTC 2018 */
 
 /* Pistachio Kernel Configuration System */
@@ -70,7 +70,7 @@
 #undef  CONFIG_KDB_ON_STARTUP
 #undef  CONFIG_KDB_BREAKIN
 #undef  CONFIG_KDB_BREAKIN_BREAK
-#undef  CONFIG_KDB_NO_ASSERTS
+#define CONFIG_KDB_NO_ASSERTS 1
 
 /* Trace Settings */
 #undef  CONFIG_VERBOSE_INIT
