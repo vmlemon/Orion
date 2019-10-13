8349595813ea1a059f753bed0297ab0511df4025
Tyson Key <tyson.key@gmail.com>
2019-10-13 12:50:33 +0100

    Hack to try with Python 3

--- contrib/cml2/cmlconfigure.py
+++ contrib/cml2/cmlconfigure.py
@@ -7,7 +7,10 @@
 #
 import sys
 
-if sys.version[0] < '2':
+if (sys.version_info > (3, 0)):
+	print("Python 3 support is experimental, and is not guaranteed to work.")
+	
+else if sys.version[0] < '2':
     print "Python 2.0 or later is required for this program."
     raise SystemExit, 1
 
