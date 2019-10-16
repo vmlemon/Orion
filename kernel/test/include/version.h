c04100d9c129b8c63a2cac975ac30d3e24218f1f
Tyson Key <tyson.key@gmail.com>
2019-10-16 01:15:18 +0100

    Try to build x86-64 userland

--- kernel/test/include/version.h
+++ kernel/test/include/version.h
@@ -14,9 +14,9 @@
 #define KERNEL_SUBSUBVERSION   0			
 
 \
-#define KERNELGENDATE          SHUFFLE3(day:(115-100),month:(110-100),year:(2019-2000))			
+#define KERNELGENDATE          SHUFFLE3(day:(116-100),month:(110-100),year:(2019-2000))			
 \
 #define __USER__               "tyson@tyson-Lenovo-ideapad-120S-14IAP"				
 \
-#define __KERNELBUILDNO__      1				
+#define __KERNELBUILDNO__      2				
 
