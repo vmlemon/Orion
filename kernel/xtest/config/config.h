c315f8b2345f446390d7a5861c844799d514a408
Tyson Key <tyson.key@gmail.com>
2019-10-21 23:58:15 +0100

    Try to build

--- kernel/xtest/config/config.h
+++ kernel/xtest/config/config.h
@@ -1,1 +1,108 @@
-/* machine-generated file - do not edit */
+/* Automatically generated, don't edit */
+/* Generated on: tyson-Lenovo-ideapad-120S-14IAP */
+/* At: Mon, 21 Oct 2019 22:58:03 +0000 */
+/* Linux version 5.0.0-31-generic (buildd@lcy01-amd64-010) (gcc version 8.3.0 (Ubuntu 8.3.0-6ubuntu1)) #33-Ubuntu SMP Mon Sep 30 18:51:59 UTC 2019 */
+
+/* Pistachio Kernel Configuration System */
+
+/* Hardware */
+
+/* Basic Architecture */
+#define CONFIG_ARCH_IA32 1
+#undef  CONFIG_ARCH_IA64
+#undef  CONFIG_ARCH_POWERPC
+#undef  CONFIG_ARCH_POWERPC64
+#undef  CONFIG_ARCH_AMD64
+#undef  CONFIG_ARCH_ALPHA
+#undef  CONFIG_ARCH_MIPS64
+#undef  CONFIG_ARCH_ARM
+#undef  CONFIG_ARCH_SPARC64
+
+
+/* Processor Type */
+#undef  CONFIG_CPU_IA32_I486
+#undef  CONFIG_CPU_IA32_I586
+#undef  CONFIG_CPU_IA32_I686
+#undef  CONFIG_CPU_IA32_P4
+#undef  CONFIG_CPU_IA32_K8
+#undef  CONFIG_CPU_IA32_C3
+
+
+/* Processor Type */
+#undef  CONFIG_CPU_AMD64_OPTERON
+#undef  CONFIG_CPU_AMD64_SIMICS
+
+
+/* Platform */
+#define CONFIG_PLAT_PC99 1
+
+
+/* Platform */
+#undef  CONFIG_PLAT_OFG5
+#undef  CONFIG_PLAT_OFPOWER3
+#undef  CONFIG_PLAT_OFPOWER4
+
+
+/* Processor Type */
+#undef  CONFIG_CPU_POWERPC64_POWER3
+#undef  CONFIG_CPU_POWERPC64_POWER3p
+#undef  CONFIG_CPU_POWERPC64_POWER4
+#undef  CONFIG_CPU_POWERPC64_POWER4p
+#undef  CONFIG_CPU_POWERPC64_PPC970
+
+#undef  CONFIG_SMP
+
+/* Miscellaneous */
+#undef  CONFIG_IOAPIC
+#undef  CONFIG_DISABLE_ALIGNMENT_EXCEPTIONS
+
+
+
+/* Kernel */
+#undef  CONFIG_IPC_FASTPATH
+#define CONFIG_DEBUG 1
+#undef  CONFIG_IA32_SMALL_SPACES
+#undef  CONFIG_SPIN_WHEELS
+
+
+/* Debugger */
+#undef  CONFIG_KDB
+#undef  CONFIG_KDB_NO_ASSERTS
+#define CONFIG_ASSERT_LEVEL 2
+#undef  CONFIG_DEBUG_SYMBOLS
+
+/* Trace Settings */
+#undef  CONFIG_TRACEBUFFER
+
+
+
+/* Code Generator Options */
+
+
+/* Derived symbols */
+#undef  CONFIG_IA32_FXSR
+#undef  CONFIG_IA32_PGE
+#undef  CONFIG_IS_64BIT
+#undef  CONFIG_PLAT_OFSPARC64
+#undef  CONFIG_IA32_HTT
+#undef  CONFIG_BIGENDIAN
+#undef  CONFIG_SPARC64_SAB82532
+#define CONFIG_IS_32BIT 1
+#undef  CONFIG_CPU_SPARC64_ULTRASPARC
+#undef  CONFIG_ARM_BIG_ENDIAN
+#undef  CONFIG_SWIZZLE_IO_ADDR
+#undef  CONFIG_IA32_SMALL_SPACES_GLOBAL
+#undef  CONFIG_HAVE_MEMORY_CONTROL
+#undef  CONFIG_IA32_PSE
+#undef  CONFIG_ARM_V5
+#undef  CONFIG_SPARC64_ULTRASPARC2I
+#undef  CONFIG_ARM_THUMB_SUPPORT
+#undef  CONFIG_IA32_TSC
+#undef  CONFIG_SPARC64_ULTRASPARC1
+#undef  CONFIG_ACPI
+#undef  CONFIG_SPARC64_Z8530
+#undef  CONFIG_ALPHA_FASTPATH
+#undef  CONFIG_SPARC64_ULTRASPARC2
+#undef  CONFIG_IA32_SYSENTER
+/* That's all, folks! */
+#define AUTOCONF_INCLUDED
