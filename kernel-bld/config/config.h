6e21aa152ca89e5218e09b9d870cefb66deeb768
Tyson Key <tyson.key@gmail.com>
2020-01-25 18:31:24 +0000

    Again, again

--- kernel-bld/config/config.h
+++ kernel-bld/config/config.h
@@ -1,6 +1,6 @@
 /* Automatically generated, don't edit */
 /* Generated on: tyson-Lenovo-ideapad-120S-14IAP */
-/* At: Sat, 25 Jan 2020 18:18:55 +0000 */
+/* At: Sat, 25 Jan 2020 18:30:25 +0000 */
 /* Linux version 5.0.0-38-generic (buildd@lgw01-amd64-036) (gcc version 8.3.0 (Ubuntu 8.3.0-6ubuntu1)) #41-Ubuntu SMP Tue Dec 3 00:27:35 UTC 2019 */
 
 /* Pistachio Kernel Configuration System */
@@ -8,11 +8,11 @@
 /* Hardware */
 
 /* Basic Architecture */
-#undef  CONFIG_ARCH_IA32
+#define CONFIG_ARCH_IA32 1
 #undef  CONFIG_ARCH_IA64
 #undef  CONFIG_ARCH_POWERPC
 #undef  CONFIG_ARCH_POWERPC64
-#define CONFIG_ARCH_AMD64 1
+#undef  CONFIG_ARCH_AMD64
 #undef  CONFIG_ARCH_ALPHA
 #undef  CONFIG_ARCH_MIPS64
 #undef  CONFIG_ARCH_ARM
@@ -23,7 +23,7 @@
 #undef  CONFIG_CPU_IA32_I486
 #undef  CONFIG_CPU_IA32_I586
 #undef  CONFIG_CPU_IA32_I686
-#undef  CONFIG_CPU_IA32_P4
+#define CONFIG_CPU_IA32_P4 1
 #undef  CONFIG_CPU_IA32_K8
 #undef  CONFIG_CPU_IA32_C3
 
@@ -35,7 +35,7 @@
 
 
 /* Processor Type */
-#define CONFIG_CPU_AMD64_OPTERON 1
+#undef  CONFIG_CPU_AMD64_OPTERON
 #undef  CONFIG_CPU_AMD64_SIMICS
 
 
@@ -46,6 +46,7 @@
 /* IA-64 Platform */
 #define CONFIG_PLAT_EFI 1
 
+#undef  CONFIG_SMP
 
 /* Miscellaneous */
 #define CONFIG_IOAPIC 1
@@ -58,6 +59,7 @@
 /* Kernel */
 #define CONFIG_IPC_FASTPATH 1
 #define CONFIG_DEBUG 1
+#undef  CONFIG_IA32_SMALL_SPACES
 #undef  CONFIG_PERFMON
 #undef  CONFIG_SPIN_WHEELS
 
@@ -78,29 +80,29 @@
 
 
 /* Derived symbols */
-#undef  CONFIG_IA32_FXSR
-#undef  CONFIG_IA32_PGE
-#define CONFIG_IS_64BIT 1
+#define CONFIG_IA32_FXSR 1
+#define CONFIG_IA32_PGE 1
+#undef  CONFIG_IS_64BIT
 #undef  CONFIG_PLAT_OFSPARC64
-#undef  CONFIG_IA32_HTT
+#define CONFIG_IA32_HTT 1
 #undef  CONFIG_BIGENDIAN
 #undef  CONFIG_SPARC64_SAB82532
-#undef  CONFIG_IS_32BIT
+#define CONFIG_IS_32BIT 1
 #undef  CONFIG_CPU_SPARC64_ULTRASPARC
 #undef  CONFIG_ARM_BIG_ENDIAN
 #undef  CONFIG_SWIZZLE_IO_ADDR
 #undef  CONFIG_IA32_SMALL_SPACES_GLOBAL
 #undef  CONFIG_HAVE_MEMORY_CONTROL
-#undef  CONFIG_IA32_PSE
+#define CONFIG_IA32_PSE 1
 #undef  CONFIG_ARM_V5
 #undef  CONFIG_SPARC64_ULTRASPARC2I
 #undef  CONFIG_ARM_THUMB_SUPPORT
-#undef  CONFIG_IA32_TSC
+#define CONFIG_IA32_TSC 1
 #undef  CONFIG_SPARC64_ULTRASPARC1
 #undef  CONFIG_ACPI
 #undef  CONFIG_SPARC64_Z8530
 #define CONFIG_ALPHA_FASTPATH 1
 #undef  CONFIG_SPARC64_ULTRASPARC2
-#undef  CONFIG_IA32_SYSENTER
+#define CONFIG_IA32_SYSENTER 1
 /* That's all, folks! */
 #define AUTOCONF_INCLUDED
