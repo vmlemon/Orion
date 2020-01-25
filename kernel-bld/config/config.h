acf81bdc113053abf9e6abefd84dbc69503e313f
Tyson Key <tyson.key@gmail.com>
2020-01-25 18:19:21 +0000

    Try to configure for x86-64, again

--- kernel-bld/config/config.h
+++ kernel-bld/config/config.h
@@ -1,1 +1,106 @@
-/* machine-generated file - do not edit */
+/* Automatically generated, don't edit */
+/* Generated on: tyson-Lenovo-ideapad-120S-14IAP */
+/* At: Sat, 25 Jan 2020 18:18:55 +0000 */
+/* Linux version 5.0.0-38-generic (buildd@lgw01-amd64-036) (gcc version 8.3.0 (Ubuntu 8.3.0-6ubuntu1)) #41-Ubuntu SMP Tue Dec 3 00:27:35 UTC 2019 */
+
+/* Pistachio Kernel Configuration System */
+
+/* Hardware */
+
+/* Basic Architecture */
+#undef  CONFIG_ARCH_IA32
+#undef  CONFIG_ARCH_IA64
+#undef  CONFIG_ARCH_POWERPC
+#undef  CONFIG_ARCH_POWERPC64
+#define CONFIG_ARCH_AMD64 1
+#undef  CONFIG_ARCH_ALPHA
+#undef  CONFIG_ARCH_MIPS64
+#undef  CONFIG_ARCH_ARM
+#undef  CONFIG_ARCH_SPARC64
+
+
+/* Processor Type */
+#undef  CONFIG_CPU_IA32_I486
+#undef  CONFIG_CPU_IA32_I586
+#undef  CONFIG_CPU_IA32_I686
+#undef  CONFIG_CPU_IA32_P4
+#undef  CONFIG_CPU_IA32_K8
+#undef  CONFIG_CPU_IA32_C3
+
+
+/* IA-64 Processor Type */
+#define CONFIG_CPU_IA64_ITANIUM 1
+#undef  CONFIG_CPU_IA64_ITANIUM2
+#undef  CONFIG_CPU_IA64_SKI
+
+
+/* Processor Type */
+#define CONFIG_CPU_AMD64_OPTERON 1
+#undef  CONFIG_CPU_AMD64_SIMICS
+
+
+/* Platform */
+#define CONFIG_PLAT_PC99 1
+
+
+/* IA-64 Platform */
+#define CONFIG_PLAT_EFI 1
+
+
+/* Miscellaneous */
+#define CONFIG_IOAPIC 1
+#define CONFIG_MAX_IOAPICS 2
+#define CONFIG_APIC_TIMER_TICK 1000
+#undef  CONFIG_DISABLE_ALIGNMENT_EXCEPTIONS
+
+
+
+/* Kernel */
+#define CONFIG_IPC_FASTPATH 1
+#define CONFIG_DEBUG 1
+#undef  CONFIG_PERFMON
+#undef  CONFIG_SPIN_WHEELS
+
+
+/* Debugger */
+#undef  CONFIG_KDB
+#undef  CONFIG_KDB_NO_ASSERTS
+#define CONFIG_ASSERT_LEVEL 2
+#define CONFIG_DEBUG_SYMBOLS 1
+
+/* Trace Settings */
+#define CONFIG_TRACEBUFFER 1
+#define CONFIG_TRACEBUF_PAGES 256
+
+
+
+/* Code Generator Options */
+
+
+/* Derived symbols */
+#undef  CONFIG_IA32_FXSR
+#undef  CONFIG_IA32_PGE
+#define CONFIG_IS_64BIT 1
+#undef  CONFIG_PLAT_OFSPARC64
+#undef  CONFIG_IA32_HTT
+#undef  CONFIG_BIGENDIAN
+#undef  CONFIG_SPARC64_SAB82532
+#undef  CONFIG_IS_32BIT
+#undef  CONFIG_CPU_SPARC64_ULTRASPARC
+#undef  CONFIG_ARM_BIG_ENDIAN
+#undef  CONFIG_SWIZZLE_IO_ADDR
+#undef  CONFIG_IA32_SMALL_SPACES_GLOBAL
+#undef  CONFIG_HAVE_MEMORY_CONTROL
+#undef  CONFIG_IA32_PSE
+#undef  CONFIG_ARM_V5
+#undef  CONFIG_SPARC64_ULTRASPARC2I
+#undef  CONFIG_ARM_THUMB_SUPPORT
+#undef  CONFIG_IA32_TSC
+#undef  CONFIG_SPARC64_ULTRASPARC1
+#undef  CONFIG_ACPI
+#undef  CONFIG_SPARC64_Z8530
+#define CONFIG_ALPHA_FASTPATH 1
+#undef  CONFIG_SPARC64_ULTRASPARC2
+#undef  CONFIG_IA32_SYSENTER
+/* That's all, folks! */
+#define AUTOCONF_INCLUDED
